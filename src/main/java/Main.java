a385fe70d37dc3091de7ca7b0dbd38632b41d836
dhirajrathod <dhirajrathod3000@gmail.com>
2021-08-02 22:58:33 +0530

    ADDED: To validate User First Name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 /**
  *  Main Class
  */
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean validName;
+
+        //First name input
+        do {
+            System.out.printf("Enter a First Name of User: ");
+            String firstName = sc.nextLine();
+            validName = RegexCheckingOperations.usernameValidationChecking(firstName);
+        }
+        while (validName == false);
+    }
 }
--- src/main/java/RegexCheckingOperations.java
+++ src/main/java/RegexCheckingOperations.java
@@ -1,2 +1,19 @@
+import java.util.regex.Pattern;
+
 public class RegexCheckingOperations {
+    /**
+     * Username Validation Checking Method
+     * @param name
+     * @return
+     */
+    public static boolean usernameValidationChecking(String name){
+        boolean validation = Pattern.matches("^[A-Z][a-z]{2,}",name);
+        if(validation){
+            System.out.println(name+" is Valid name");
+        }
+        else {
+            System.out.println(name+" is Invalid name");
+        }
+        return validation;
+    }
 }
