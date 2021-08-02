7b935c6cb518ac65ecaab450e9ab60956dcc785a
dhirajrathod <dhirajrathod3000@gmail.com>
2021-08-03 00:47:17 +0530

    ADDED: Junit test Validate name email phone number password

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,10 @@
+public class MoodAnalyzer {
+    public String analyzeMood(String message){
+        if(message.contains("sad")){
+            return "SAD";
+        }
+        else {
+            return "HAPPY";
+        }
+    }
+}
